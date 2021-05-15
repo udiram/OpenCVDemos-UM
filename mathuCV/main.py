1383232585103625eac0d8fa0d265dc29e067a59
mathunada07 <mathunada07@gmail.com>
2021-05-14 21:25:48 -0400

    Switch to Macbook project

--- mathuCV/main.py
+++ mathuCV/main.py
@@ -1,22 +1,46 @@
 import cv2
-import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
-import pytesseract
-from PIL import Image
 
-if __name__ == "__main__":
+image = cv2.imread("greenThingsRemastered.png")
 
-    img = cv.imread(r'C:\Users\sathy\PycharmProjects\mathuCV\greenThingsRemastered.png')
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+lower_range = np.array([40, 0, 0])
+upper_range = np.array([70, 255, 255])
 
-    lower_range = np.array([40, 0, 0])
-    upper_range = np.array([70, 255, 255])
+mask = cv2.inRange(hsv, lower_range, upper_range)
 
-    mask = cv.inRange(hsv, lower_range, upper_range)
+cv2.imwrite("maskog.jpg", mask)
 
-    cv.imshow("img", img)
-    cv.imshow("mask", mask)
-    cv.imwrite(r"C:\Users\sathy\PycharmProjects\mathuCV\maskog.jpg", mask)
-    cv.waitKey(0)
+# =======================================================
+# read image
+img = cv2.imread("maskog.jpg")
+
+# convert to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# threshold
+thresh = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)[1]
+
+# get contours
+result = img.copy()
+contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours = contours[0] if len(contours) == 2 else contours[1]
+i = 0
+for cntr in contours:
+    x, y, w, h = cv2.boundingRect(cntr)
+    cv2.rectangle(result, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    print("x,y,w,h:", x, y, w, h)
+    if w*h > 20:
+        i = i+1
+    print(str(i))
+# save resulting image
+
+result = cv2.putText(result, str(i), (0,50), cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0,255,0),thickness=3 )
+cv2.imwrite('two_blobs_result.jpg', result)
+
+# show thresh and result
+cv2.imshow("bounding_box", result)
+cv2.imshow("img", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
